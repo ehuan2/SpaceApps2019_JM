ee19d3fc886ac44b40d4ce7a101cfd59a8982f4f
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2019-10-19 01:33:41 -0400

    Revert "Added some of the needed source files, classes that I think we might need"
    This reverts commit 9c44cc737fd6ca2c21c3e19d5feacd84a0e6a014.

--- src/CityGame.java
+++
@@ -1,9 +0,0 @@
-
-public class CityGame extends City {
-
-    // the citygame is part of city, but has added stuff like citizen happiness
-    // economy at a level, citizen
-
-
-
-}
--- src/Menu.java
+++
@@ -1,6 +0,0 @@
-
-public class Menu {
-    // create the ui lol
-    // should make it to either map, or to game part
-
-}
