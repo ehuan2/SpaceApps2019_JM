38b955cbbad14488d36ed3f490bf9f0877c2edc1
Liam MacHale <44303243+liammac1@users.noreply.github.com>
2019-10-19 08:03:41 -0400

    Update City.java

--- Space_Apps_2019/src/City.java
+++ Space_Apps_2019/src/City.java
@@ -5,22 +5,20 @@ public class City {
     String name; // name
     int level; // level that it is at, if it is below sea level, will technically "drown"
     Image image; // image that will be added
-    static City[]cities; // holds all the cities, from the text file allCities,
-                            // that text file will contain from order, the name, the level then the image
+    int income; //income of cities
 
     public City(){
 
     }
 
-    public City(String nme, int sLvl){
-        name = nme;
-        level =  sLvl;
-    }
-
-    public City(String nme, int sLvl, Image img){
-        name = nme;
-        level = sLvl;
-        image = img;
+    //constructor for city
+    public City(String nme, int sLvl, Image img, int inc){
+    
+        this.name = nme;
+        this.level = sLvl;
+        this.image = img;
+        this.income = inc;
+    
     }
 
 
