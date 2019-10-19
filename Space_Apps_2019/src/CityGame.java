8efbf9f0aab02d6f386f328955ad28fb0dc038d7
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2019-10-19 09:23:16 -0400

    Added instance variables for CityGame

--- Space_Apps_2019/src/CityGame.java
+++ Space_Apps_2019/src/CityGame.java
@@ -4,6 +4,8 @@ public class CityGame extends City {
     // the citygame is part of city, but has added stuff like citizen happiness
     // economy at a level, citizen
 
+    int morale;
+    double growth;
 
 
 }
--- Space_Apps_2019/src/Player.java
+++ Space_Apps_2019/src/Player.java
@@ -1,12 +1,12 @@
 
-public class Player implements Comparable<Player>{
+public class Player implements Comparable<Player>{ // this comparable is used for the leaderboard
 
     String name;
     int score;
 
 
     @Override
-    public int compareTo(Player o) {
+    public int compareTo(Player o) { // this compareTo is again, used for the Array.sort()
         return o.score - this.score;
     }
 
