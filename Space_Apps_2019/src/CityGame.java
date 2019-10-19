a282584605bcb5e33fc9582a80994665ac6eb88f
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2019-10-19 12:31:35 -0400

    Some other changes again

--- Space_Apps_2019/src/CityGame.java
+++ Space_Apps_2019/src/CityGame.java
@@ -4,13 +4,15 @@ public class CityGame extends City {
     // the citygame is part of city, but has added stuff like citizen happiness
     // economy at a level, citizen
 
-    int morale;
+    double morale = 1.0; // between 0 and 1
     double growth; // economic growth
     double growthCitizens; // growth rate of citizens
     int land; // land mass
     int birthRate; // birth rate
     int deathRate; // death rate
     double tempRise; // temp rise based on how well the economy is doing, affects sea level
+    double minMorale = 0.75; // can change later based on difficulty
+    final int mincitizenNeed = 40000; // changes later based on difficulty
 
     // turn based year, by 1 year
     public void changeYear(){
