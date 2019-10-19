5aeff22f49ce77916a672e0889334e270c435ab8
andrewzhang30 <56743940+andrewzhang30@users.noreply.github.com>
2019-10-19 08:11:38 -0400

    Update ChooseCity.java

--- Space_Apps_2019/src/ChooseCity.java
+++ Space_Apps_2019/src/ChooseCity.java
@@ -1,2 +1,6 @@
-public class ChooseCity {
+public class ChooseCity 
+{
+  //selector screen from the menu itself to the city choices, then to a zoomed in map/game itself, should include the creation of own city (possibly)
+  
+  
 }
