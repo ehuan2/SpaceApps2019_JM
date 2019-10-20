d2e4a0db379c7ce668691ebbe18df780da580853
Liam MacHale <44303243+liammac1@users.noreply.github.com>
2019-10-20 11:33:17 -0400

    Update Main.java

--- Space_Apps_2019/src/Main.java
+++ Space_Apps_2019/src/Main.java
@@ -1,6 +1,8 @@
-import java.util.*;
-import javax.swing.*;
+
 import java.awt.*;
+import javax.swing.*;
+
+import java.util.Timer;
 
 public class Main {
 
@@ -17,6 +19,20 @@ public class Main {
     static JLabel citMor = new JLabel("Citizen Morale: ");
     static JButton upEcon = new JButton("Upgrade Economy");
     static JButton upWall = new JButton("Upgrade Sea Wall");
+    static Image houst;// = new ImageIcon(Main.class.getResource("houston.PNG")).getImage();
+    static Image johns;// = new ImageIcon(Main.class.getResource("stJohns.PNG")).getImage();
+    static Image miami;// = new ImageIcon(Main.class.getResource("miami.PNG")).getImage();
+    static Image losA;// = new ImageIcon(Main.class.getResource("losAngeles.PNG")).getImage();
+    static Image nyc;// = new ImageIcon(Main.class.getResource("newYork.PNG")).getImage();
+    static Image van;// = new ImageIcon(Main.class.getResource("vancouver.PNG")).getImage();
+    static Image sanFran;// = new ImageIcon(Main.class.getResource("sanfran.PNG")).getImage();
+    static Image bos;// = new ImageIcon(Main.class.getResource("boston.PNG")).getImage();
+    static Image newOr;// = new ImageIcon(Main.class.getResource("newOrleans.PNG")).getImage();
+    static Image vic;// = new ImageIcon(Main.class.getResource("vic.PNG")).getImage();
+    static Image hon;// = new ImageIcon(Main.class.getResource("hono.PNG")).getImage();
+    static Image tor;// = new ImageIcon(Main.class.getResource("toronto.PNG")).getImage();
+    static Image choice;// = new ImageIcon(Main.class.getResource("choice.jpg")).getImage();
+    static final Image[] CITY_PICS = {houst,johns,miami,losA,nyc,van,sanFran,bos,newOr,vic,hon,tor};
     static volatile CityGame userCity;
     static final int yearTime = 1;
     static City[] cities = new City[11];
@@ -31,30 +47,50 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //collecting images
+        collImages();
+
         //filling cities with actual cities
         finCits();
 
         //user picked city
-        userCity = new CityGame(new City("Eric", 100, 10000000, 1000000, 0), 100, 100, 100, .8);
+        //userCity = new CityGame(new City("Eric", 100, 10000000, 1000000, 0, 100, 100, 100, .8,choice);
 
         Timer timer = new Timer();
-        timer.schedule(new Timing(), 0, yearTime*1000); //30 seconds
-        
+        timer.scheduleAtFixedRate(new Timing(), 0, yearTime*1000); //30 seconds
+
         setGUI();
 
+    }
+
+    static void collImages(){
+
+        houst = new ImageIcon(Main.class.getResource("houston.PNG")).getImage();
+        johns = new ImageIcon(Main.class.getResource("stJohns.PNG")).getImage();
+        miami = new ImageIcon(Main.class.getResource("miami.PNG")).getImage();
+        losA = new ImageIcon(Main.class.getResource("losAngeles.PNG")).getImage();
+        nyc = new ImageIcon(Main.class.getResource("newYork.PNG")).getImage();
+        van = new ImageIcon(Main.class.getResource("vancouver.PNG")).getImage();
+        sanFran = new ImageIcon(Main.class.getResource("sanfran.PNG")).getImage();
+        bos = new ImageIcon(Main.class.getResource("boston.PNG")).getImage();
+        newOr = new ImageIcon(Main.class.getResource("newOrleans.PNG")).getImage();
+        vic = new ImageIcon(Main.class.getResource("vic.PNG")).getImage();
+        hon = new ImageIcon(Main.class.getResource("hono.PNG")).getImage();
+        tor = new ImageIcon(Main.class.getResource("toronto.PNG")).getImage();
+        choice = new ImageIcon(Main.class.getResource("choice.jpg")).getImage();
     }
 
     //initializing new cities
     static void finCits(){
 
         for (int i = 0; i < cities.length; i++){
-            cities[i] = new City(cityNames[i],citySeaLvls[i],cityEcons[i],cityCitz[i], seaWallHeight[i]);
+            cities[i] = new City(cityNames[i],citySeaLvls[i],cityEcons[i],cityCitz[i], seaWallHeight[i],CITY_PICS[i]);
             System.out.println(cities[i]); //for testing purposes
         }
 
 
     }
-    
+
     private static void setGUI(){
 
         frame.setSize(1400,700);
@@ -78,6 +114,12 @@ public class Main {
         mAbove.setBounds(860,0,200,17);
         mAbove.setFont(new Font("Dialog",Font.BOLD,16));
         mainPanel.add(mAbove);
+
+        rising.setBounds(10,frame.getHeight()-10,100,17);
+        rising.setFont(new Font("Dialog",Font.BOLD,16));
+        mainPanel.add(rising);
+
+
 
     }
 }
