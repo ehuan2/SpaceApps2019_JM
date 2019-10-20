d3cdc951ddbd18da1b40bd0bfb146fb7d13afaa2
andrewzhang30 <56743940+andrewzhang30@users.noreply.github.com>
2019-10-20 14:15:34 -0400

    Update Main.java

--- Space_Apps_2019/src/Main.java
+++ Space_Apps_2019/src/Main.java
@@ -1,24 +1,34 @@
 
-import java.awt.*;
-import javax.swing.*;
-
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.util.Timer;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 public class Main {
 
-    static JFrame frame = new JFrame();
+	static JFrame frame = new JFrame();
     static JPanel sideBar = new JPanel(null);
     static JPanel mainPanel = new JPanel(null);
     static JSplitPane spliter = new JSplitPane();
     static JLabel seaLvl = new JLabel("Sea Level: ");
     static JLabel rising = new JLabel("Rate of Rising Water: ");
-    static JLabel mAbove = new JLabel("Metres above sea level: ");
+    static JLabel mAbove = new JLabel("Metres Above Sea Level: ");
     static JButton btnNext = new JButton("Next Year");
     static JLabel popSize = new JLabel("Current Population: ");
     static JLabel currMoney = new JLabel("$");
     static JLabel citMor = new JLabel("Citizen Morale: ");
     static JButton upEcon = new JButton("Upgrade Economy");
     static JButton upWall = new JButton("Upgrade Sea Wall");
+    static Color moraleStatus=Color.GREEN;
+    
     static Image houst;// = new ImageIcon(Main.class.getResource("houston.PNG")).getImage();
     static Image johns;// = new ImageIcon(Main.class.getResource("stJohns.PNG")).getImage();
     static Image miami;// = new ImageIcon(Main.class.getResource("miami.PNG")).getImage();
@@ -34,7 +44,7 @@ public class Main {
     static Image choice;// = new ImageIcon(Main.class.getResource("choice.jpg")).getImage();
     static final Image[] CITY_PICS = {houst,johns,miami,losA,nyc,van,sanFran,bos,newOr,vic,hon,tor};
     static volatile CityGame userCity;
-    static final int yearTime = 10; // they only get thirty seconds to decide to add or not
+    static final int yearTime = 1;
     static City[] cities = new City[11];
     static String[] cityNames = {"Houston","St. Johns","Miami","Los Angeles","New York City","Vancouver","San " +
             "Fransisco","Boston","New Orleans","Victoria", "Honolulu","Toronto"};
@@ -44,11 +54,11 @@ public class Main {
     static int[] cityCitz = {2000000,2000000,500000,3000000,1000000,6000000,1000000,2000000,500000,1000000,600000,
             3000000};
     static int[] seaWallHeight = {5,5,1,2,5,3,2,0,6,4,8,0};
-
-    public static void main(String[] args) {
-
-        //collecting images
-        collImages();
+    static JLabel Background;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		collImages();
 
         //filling cities with actual cities
         finCits();
@@ -60,26 +70,25 @@ public class Main {
         timer.scheduleAtFixedRate(new Timing(), 0, yearTime*1000); //30 seconds
 
         setGUI();
+	}
+	
+	static void collImages(){
 
+        //houst = new ImageIcon(Main.class.getResource("houston.PNG")).getImage();
+        //johns = new ImageIcon(Main.class.getResource("stJohns.PNG")).getImage();
+        //miami = new ImageIcon(Main.class.getResource("miami.PNG")).getImage();
+        //losA = new ImageIcon(Main.class.getResource("losAngeles.PNG")).getImage();
+        //nyc = new ImageIcon(Main.class.getResource("newYork.PNG")).getImage();
+        //van = new ImageIcon(Main.class.getResource("vancouver.PNG")).getImage();
+        //sanFran = new ImageIcon(Main.class.getResource("sanfran.PNG")).getImage();
+        //bos = new ImageIcon(Main.class.getResource("boston.png")).getImage();
+        newOr = new ImageIcon(Main.class.getResource("newOrleans.png")).getImage();
+        //vic = new ImageIcon(Main.class.getResource("vic.PNG")).getImage();
+        //hon = new ImageIcon(Main.class.getResource("hono.PNG")).getImage();
+        //tor = new ImageIcon(Main.class.getResource("toronto.PNG")).getImage();
+        //choice = new ImageIcon(Main.class.getResource("choice.jpg")).getImage();
     }
-
-    static void collImages(){
-
-        houst = new ImageIcon(Main.class.getResource("houston.PNG")).getImage();
-        johns = new ImageIcon(Main.class.getResource("stJohns.PNG")).getImage();
-        miami = new ImageIcon(Main.class.getResource("miami.PNG")).getImage();
-        losA = new ImageIcon(Main.class.getResource("losAngeles.PNG")).getImage();
-        nyc = new ImageIcon(Main.class.getResource("newYork.PNG")).getImage();
-        van = new ImageIcon(Main.class.getResource("vancouver.PNG")).getImage();
-        sanFran = new ImageIcon(Main.class.getResource("sanfran.PNG")).getImage();
-        bos = new ImageIcon(Main.class.getResource("boston.PNG")).getImage();
-        newOr = new ImageIcon(Main.class.getResource("newOrleans.PNG")).getImage();
-        vic = new ImageIcon(Main.class.getResource("vic.PNG")).getImage();
-        hon = new ImageIcon(Main.class.getResource("hono.PNG")).getImage();
-        tor = new ImageIcon(Main.class.getResource("toronto.PNG")).getImage();
-        choice = new ImageIcon(Main.class.getResource("choice.jpg")).getImage();
-    }
-
+	
     //initializing new cities
     static void finCits(){
 
@@ -98,28 +107,91 @@ public class Main {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        sideBar.setBackground(new Color(128,128,128));
-
+        sideBar.setBackground(new Color(0,105,148));
+        
         spliter.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
         spliter.setDividerLocation(1100);
+
+        spliter.setDividerSize(0);
+        
         spliter.setLeftComponent(mainPanel);
         spliter.setRightComponent(sideBar);
         spliter.setEnabled(false);
         frame.add(spliter);
 
-        seaLvl.setBounds(10,0,100,17);
+        seaLvl.setBounds(10,10,100,17);
+        seaLvl.setForeground(Color.WHITE);
         seaLvl.setFont(new Font("Dialog",Font.BOLD,16));
-        mainPanel.add(seaLvl);
+        sideBar.add(seaLvl);
 
-        mAbove.setBounds(860,0,200,17);
+        mAbove.setBounds(10,40,200,17);
+        mAbove.setForeground(Color.WHITE);
         mAbove.setFont(new Font("Dialog",Font.BOLD,16));
-        mainPanel.add(mAbove);
+        sideBar.add(mAbove);
 
-        rising.setBounds(10,frame.getHeight()-10,100,17);
+        rising.setBounds(10,70,200,17);
+        rising.setForeground(Color.WHITE);
         rising.setFont(new Font("Dialog",Font.BOLD,16));
-        mainPanel.add(rising);
-
-
-
+        sideBar.add(rising);
+                
+        popSize.setBounds(10, 620, 200, 17);
+        popSize.setForeground(Color.WHITE);
+        popSize.setFont(new Font("Dialog", Font.BOLD, 16));
+        sideBar.add(popSize);
+        
+        currMoney.setBounds(10, 590, 200, 17);
+        currMoney.setForeground(Color.WHITE);
+        currMoney.setFont(new Font("Dialog", Font.BOLD, 16));
+        sideBar.add(currMoney);
+        
+        citMor.setBounds(10, 560, 200, 17);
+        citMor.setForeground(moraleStatus);
+        citMor.setFont(new Font("Dialog", Font.BOLD, 16));
+        sideBar.add(citMor);
+        
+        btnNext.setBounds(30, 120, 230, 100);
+        //btnNext.setContentAreaFilled(false);
+        //btnNext.setOpaque(false);
+        btnNext.setBackground(new Color(11, 61, 145));
+        btnNext.setForeground(Color.WHITE);
+        btnNext.setFocusable(false);
+        btnNext.setFont(new Font("Dialog", Font.ITALIC | Font.BOLD, 25));
+        btnNext.setBorderPainted(false);
+        sideBar.add(btnNext);
+        
+        upEcon.setBounds(30, 260, 230, 100);
+        //upEcon.setOpaque(false);
+        upEcon.setBackground(new Color(11, 61, 145));
+        //upEcon.setContentAreaFilled(false);
+        upEcon.setForeground(Color.WHITE);
+        upEcon.setFocusable(false);
+        upEcon.setFont(new Font("Dialog", Font.ITALIC | Font.BOLD, 20));
+        upEcon.setBorderPainted(false);
+        sideBar.add(upEcon);
+        
+        upWall.setBounds(30, 400, 230, 100);
+        //upWall.setOpaque(false);
+        upWall.setBackground(new Color(11, 61, 145));
+        //upWall.setContentAreaFilled(false);
+        upWall.setForeground(Color.WHITE);
+        upWall.setFocusable(false);
+        upWall.setFont(new Font("Dialog", Font.ITALIC | Font.BOLD, 20));
+        upWall.setBorderPainted(false);
+        sideBar.add(upWall);
+        
+		Background=new JLabel();
+		Background.setIcon(new ImageIcon(newOr));
+		Background.setBounds(0, 0, 1100, 700);
+		mainPanel.add(Background);
     }
+    
+    /*
+	static Image resizeImage(Image img, int width, int height)
+	{
+		@SuppressWarnings("static-access")
+		Image resizedImage=img.getScaledInstance(width, height, img.SCALE_SMOOTH);
+		
+		return resizedImage;
+	}
+	*/
 }
