b9c75e165a571fa2024c3289da8b7c68602668f2
andrewzhang30 <56743940+andrewzhang30@users.noreply.github.com>
2019-10-19 23:35:47 -0400

    Update VidIntro.java

--- Space_Apps_2019/src/Prototyping/src/VidIntro.java
+++ Space_Apps_2019/src/Prototyping/src/VidIntro.java
@@ -56,7 +56,7 @@ public class VidIntro extends JPanel implements ActionListener
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
-		if (secs>=4.20)
+		if (secs>=4.23)
 		{
 			timer.stop();
 			
