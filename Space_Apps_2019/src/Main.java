003822ad40daf913a5f4f9eb298d0979ea732c3f
Liam MacHale <44303243+liammac1@users.noreply.github.com>
2019-10-20 10:13:58 -0400

    Update Main.java

--- Space_Apps_2019/src/Main.java
+++ Space_Apps_2019/src/Main.java
@@ -2,6 +2,19 @@ import java.util.*;
 
 public class Main {
 
+    static JFrame frame = new JFrame();
+    static JPanel sideBar = new JPanel(null);
+    static JPanel mainPanel = new JPanel(null);
+    static JSplitPane spliter = new JSplitPane();
+    static JLabel seaLvl = new JLabel("Sea Level: ");
+    static JLabel rising = new JLabel("Rate of Rising Water: ");
+    static JLabel mAbove = new JLabel("Metres above sea level: ");
+    static JButton btnNext = new JButton("Next Year");
+    static JLabel popSize = new JLabel("Current Population: ");
+    static JLabel currMoney = new JLabel("$");
+    static JLabel citMor = new JLabel("Citizen Morale: ");
+    static JButton upEcon = new JButton("Upgrade Economy");
+    static JButton upWall = new JButton("Upgrade Sea Wall");
     static volatile CityGame userCity;
     static final int yearTime = 1;
     static City[] cities = new City[11];
@@ -24,6 +37,8 @@ public class Main {
 
         Timer timer = new Timer();
         timer.schedule(new Timing(), 0, yearTime*1000); //30 seconds
+        
+        setGUI();
 
     }
 
@@ -35,6 +50,32 @@ public class Main {
             System.out.println(cities[i]); //for testing purposes
         }
 
+
+    }
+    
+    private static void setGUI(){
+
+        frame.setSize(1400,700);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        sideBar.setBackground(new Color(128,128,128));
+
+        spliter.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+        spliter.setDividerLocation(1100);
+        spliter.setLeftComponent(mainPanel);
+        spliter.setRightComponent(sideBar);
+        spliter.setEnabled(false);
+        frame.add(spliter);
+
+        seaLvl.setBounds(10,0,100,17);
+        seaLvl.setFont(new Font("Dialog",Font.BOLD,16));
+        mainPanel.add(seaLvl);
+
+        mAbove.setBounds(860,0,200,17);
+        mAbove.setFont(new Font("Dialog",Font.BOLD,16));
+        mainPanel.add(mAbove);
 
     }
 }
